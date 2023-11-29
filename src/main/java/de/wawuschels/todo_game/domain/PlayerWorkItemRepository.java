9590d4aaa4063ac9b24520ce9b08d5234bf6da4b
Norwin Wagner <norwin@wawuschels.de>
2023-11-29 20:17:42 +0100

    Revert "first commit ToDo app with working webside"
    This reverts commit 9015880c4b0d9a52fbd6196d6fccb33f282949a9.

--- src/main/java/de/wawuschels/todo_game/domain/PlayerWorkItemRepository.java
+++
@@ -1,9 +0,0 @@
-package de.wawuschels.todo_game.domain;
-
-import de.wawuschels.todo_game.domain.model.PlayerWorkItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlayerWorkItemRepository extends JpaRepository<PlayerWorkItem, Long> {
-}
--- src/main/java/de/wawuschels/todo_game/domain/WorkItemRepository.java
+++
@@ -1,9 +0,0 @@
-package de.wawuschels.todo_game.domain;
-
-import de.wawuschels.todo_game.domain.model.WorkItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WorkItemRepository extends JpaRepository<WorkItem, Long> {
-}
